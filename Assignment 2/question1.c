90a01614c45e4b436f4f73d76abbc2bfc651ef41
Divashjyoti Kashyap <86790500+divk123@users.noreply.github.com>
2021-10-10 08:59:36 +0530

    Create question1.c

---
+++ Assignment 2/question1.c
@@ -0,0 +1,16 @@
+//DIVASHJYOTI
+//2012060
+#include <stdio.h>
+
+int main()
+{
+  int num1,num2;
+  printf("Enter num1:\n");
+  scanf("%d",&num1);
+   printf("Enter num2:\n");
+  scanf("%d",&num2);
+ printf("ADDITION IN ARABIC SYSTEM:\n%d\n",num1+num2);
+ printf("MULTIPLICATION IN ARABIC SYSTEM:\n%d",num1*num2);
+ 
+    return 0;
+}
